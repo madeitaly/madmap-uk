b0fdf0f402f47aec770f58f413a1368598d56aca
madeitaly <matteo@madmap.uk>
2023-01-26 14:04:46 +0100

    Add npm start script and env variables

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 app.use(express.static(__dirname + "/public"));
 
+const port = process.env.port || 3000;
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 })
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log("Server running on port 3000");
 })
